92361bc6428ae4089c096468238c45c5a86c4fe5
Fedor <fedoravdeev3@gmail.com>
2023-05-10 18:01:17 +0700

    add open page video

--- video/views.py
+++ video/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse(request, 'test')
-  # return render(request, 'index.html')
+  # return HttpResponse(request, 'test')
+  return render(request, 'index.html')
--- video_stream/settings.py
+++ video_stream/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'video'
 ]
 
 MIDDLEWARE = [
@@ -62,7 +64,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'video.apps.VideoConfig'
             ],
         },
     },
